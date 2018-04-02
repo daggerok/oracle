45859f1f09d19c26ca70f13bd5e335a026f20a61
Maksim Kostromin <daggerok@gmail.com>
2018-04-02 23:46:24 +0300

    Fix CI

--- app/mvn-install-oracle-driver.bash
+++ app/mvn-install-oracle-driver.bash
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-bash ./mvnw install:install-file \
-  -Dfile=./libs/ojdbc6.jar \
-  -DgroupId=com.oracle \
-  -DartifactId=ojdbc6 \
-  -Dversion=11.2.0.4.0 \
-  -Dpackaging=jar
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+bash ${DIR}/mvnw \
+  install:install-file \
+    -Dfile=${DIR}/libs/ojdbc6.jar \
+    -DgroupId=com.oracle \
+    -DartifactId=ojdbc6 \
+    -Dversion=11.2.0.4.0 \
+    -Dpackaging=jar
+
+#mkdir -p ${DIR}/mvn-repo
+#
+#bash ${DIR}/mvnw \
+#  deploy:deploy-file \
+#    -Dfile=${DIR}/libs/ojdbc6.jar \
+#    -DgroupId=com.oracle \
+#    -DartifactId=ojdbc6 \
+#    -Dversion=11.2.0.4.0 \
+#    -Durl=file:${DIR}/mvn-repo \
+#    -DrepositoryId=mvn-repo \
+#    -DupdateReleaseInfo=true
+#
+#bash ${DIR}/mvnw \
+#  install:install-file \
+#    -Dfile=${DIR}/libs/ojdbc6.jar \
+#    -DgroupId=com.oracle \
+#    -DartifactId=ojdbc6 \
+#    -Dversion=11.2.0.4.0 \
+#    -Dpackaging=jar \
+#    -DlocalRepositoryPath=${DIR}/mvn-repo
